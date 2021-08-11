3bd6e676f5cbfd50c827cd58ba70ebada7260a00
georgehuble <nevajno23@mail.ru>
2021-08-12 00:02:01 +0300

    fix_28

--- backend/recipe_api/admin.py
+++ backend/recipe_api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Ingredients, IngredientInRecipe, Recipe, Tag
+from .models import IngredientInRecipe, Ingredients, Recipe, Tag
 
 
 @admin.register(Ingredients)
