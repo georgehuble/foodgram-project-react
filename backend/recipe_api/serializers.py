0565d575b02a675fcd2109f8381f583dbcc2a4d8
georgehuble <nevajno23@mail.ru>
2021-08-12 21:34:07 +0300

    new

--- backend/recipe_api/serializers.py
+++ backend/recipe_api/serializers.py
@@ -2,7 +2,7 @@ from follow_api.models import Favourite, Shopping
 from rest_framework import serializers
 from user_api.serializers import UserDetailSerializer
 
-from .models import Ingredient, Recipe, Tag, IngredientInRecipe
+from .models import IngredientInRecipe, Ingredient, Recipe, Tag
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -15,29 +15,6 @@ class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('id', 'name', 'measurement_unit')
         model = Ingredient
-
-#
-# class RecipeSerializer(serializers.ModelSerializer):
-#     tag = TagSerializer(read_only=True, many=True)
-#     ingredients = IngredientSerializer(read_only=True, many=True)
-#     author = UserDetailSerializer(read_only=True)
-#     is_favorited = serializers.SerializerMethodField('check_if_is_favorited')
-#     is_in_shopping_cart = serializers.SerializerMethodField('check_if_is_in_shopping_cart')
-#
-#     def check_if_is_favorited(self, obj):
-#         user = self.context.get('request').user
-#         return Favourite.objects.filter(user=user, name=obj).exists()
-#
-#     def check_if_is_in_shopping_cart(self, obj):
-#         user = self.context.get('request').user
-#         return Shopping.objects.filter(user=user, name=obj).exists()
-#
-#     class Meta:
-#         model = Recipe
-#         fields = ('author', 'name', 'image',
-#                   'text', 'ingredients', 'tag',
-#                   'cooking_time', 'is_favorited',
-#                   'is_in_shopping_cart')
 
 
 class AddIngredientToRecipeSerializer(serializers.ModelSerializer):
