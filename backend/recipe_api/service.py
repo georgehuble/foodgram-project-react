313245a652f12a77c7b283e044a89796f2843743
georgehuble <nevajno23@mail.ru>
2021-09-26 00:19:04 +0300

    nginx

--- backend/recipe_api/service.py
+++ backend/recipe_api/service.py
@@ -19,8 +19,10 @@ class RecipeFilter(filters.FilterSet):
     tags = CharFilterInFilter(field_name='tags__slug', lookup_expr='in')
     author = CharFilterInFilter(field_name='author', lookup_expr='in')
     name = CharFilterInFilter(field_name='name', lookup_expr='in')
-    is_favorited = CharFilterInFilter(field_name='is_favorited', lookup_expr='in')
-    is_in_shopping_cart = CharFilterInFilter(field_name='is_in_shopping_cart', lookup_expr='in')
+    is_favorited = CharFilterInFilter(field_name='is_favorited',
+                                      lookup_expr='in')
+    is_in_shopping_cart = CharFilterInFilter(field_name='is_in_shopping_cart',
+                                             lookup_expr='in')
 
     class Meta:
         model = Recipe
