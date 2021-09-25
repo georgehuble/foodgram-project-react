0715455a2bdfe77e6dff2f18f25685cf7b61378f
georgehuble <nevajno23@mail.ru>
2021-09-25 19:57:37 +0300

    image upload=''

--- backend/recipe_api/views.py
+++ backend/recipe_api/views.py
@@ -1,16 +1,11 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import mixins, permissions
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import GenericViewSet
 
 from .models import Ingredient, Recipe, Tag
 from .permissions import MyCustomPermission
 from .serializers import IngredientSerializer, RecipeSerializer, TagSerializer
 from .service import IngridientFilter
-
-
-class TalentSearchpagination(PageNumberPagination):
-    page_size = 6
 
 
 class MixinsViewSet(mixins.CreateModelMixin,
@@ -26,9 +21,10 @@ class RecipeListView(MixinsViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
     filter_backends = (DjangoFilterBackend,)
-    filterset_fields = ('author', 'name', 'tags')
+    filterset_fields = ('author', 'name', 'tags',
+                        'is_favorited',
+                        'is_in_shopping_cart')
     permission_classes = [MyCustomPermission]
-    pagination_class = TalentSearchpagination
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
