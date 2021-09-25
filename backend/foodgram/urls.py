57d653c78f014bc1930885968809b8fa30039cc8
georgehuble <nevajno23@mail.ru>
2021-09-25 15:19:59 +0300

    image nginx

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,3 +11,8 @@ urlpatterns = [
     path('api/auth/', include('djoser.urls')),
     path('api/auth/', include('djoser.urls.authtoken')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
