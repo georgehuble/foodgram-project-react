a0ce3e9d61a4e435621b1929dd4b67969abfecb3
georgehuble <nevajno23@mail.ru>
2021-10-01 23:59:55 +0300

    Final

--- backend/recipe_api/serializers.py
+++ backend/recipe_api/serializers.py
@@ -5,7 +5,6 @@ from rest_framework import serializers
 from rest_framework.serializers import ReadOnlyField
 from user_api.serializers import UserSerializer
 
-
 from .models import Ingredient, IngredientInRecipe, Recipe, Tag
 
 
